63c449ad6ea4f33e04769c6ca09a4a841510a552
Cunha <cunhanik1@gmail.com>
2022-07-26 19:02:08 +0100

    initial

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const changeThemeBtn = document.getElementById("change-theme");
+
+// Toggle dark mode
+function toggleDarkMode(){
+    document.body.classList.toggle("dark")
+}
+
+changeThemeBtn.addEventListener("change", function(){
+    toggleDarkMode();
+
+    // Save or remove dark mode
+    localStorage.removeItem("dark");
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("dark",1);
+    }
+});
+
+// Load light or dark mode
+function loadTheme(){
+    const darkMode = localStorage.getItem("dark")
+
+    if(darkMode){
+        toggleDarkMode();
+    }
+}
+
+loadTheme()
+
+
